c0fcf9ea747416fa88f2e5b2e2d90a53e5210d9d
m-shidersky <a3s59g@pochta.ru>
2016-03-23 09:15:05 +0300

    Add style add activity

--- src/com/example/faltu/MainActivity.java
+++ src/com/example/faltu/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		gestureDetector = new GestureDetector(new SwipeGestureDetector());
 		
 	}
@@ -51,6 +51,8 @@ public class MainActivity extends Activity {
 	    private static final int SWIPE_MAX_OFF_PATH = 200;
 	    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
 
+
+
 	    public boolean onFling(MotionEvent e1, MotionEvent e2,
 	                         float velocityX, float velocityY) {
 	      try {
@@ -59,17 +61,18 @@ public class MainActivity extends Activity {
 
 	        if (diffAbs > SWIPE_MAX_OFF_PATH)
 	          return false;
-	       
+
+
 	        // Left swipe
-	        if (diff > SWIPE_MIN_DISTANCE
+	      /*  if (diff > SWIPE_MIN_DISTANCE
 	        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-	           MainActivity.this.onLeftSwipe();
+	           MainActivity.this.onLeftSwipe();*/
 
 	        // Right swipe
-	        } else if (-diff > SWIPE_MIN_DISTANCE
+	      /*  } else if (-diff > SWIPE_MIN_DISTANCE
 	        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
 	        	 MainActivity.this.onRightSwipe();
-	        }
+	        }*/
 	      } catch (Exception e) {
 	        Log.e("YourActivity", "Error on gestures");
 	      }
@@ -83,6 +86,6 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
+    }
 
 }
