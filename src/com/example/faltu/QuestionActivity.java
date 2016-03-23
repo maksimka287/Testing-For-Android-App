26a97ee20bd10fefbf2ababbe2eb252f837c56c6
m-shidersky <a3s59g@pochta.ru>
2016-03-24 02:02:15 +0300

    Add profile, question, list tests

---
+++ src/com/example/faltu/QuestionActivity.java
@@ -0,0 +1,18 @@
+package com.example.faltu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MaKsa on 24.03.16.
+ */
+public class QuestionActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.right);
+
+    }
+
+}
