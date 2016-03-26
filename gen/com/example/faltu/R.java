74f47c3317c4568afb756dca99e886675c1d9c5b
m-shidersky <a3s59g@pochta.ru>
2016-03-27 03:56:31 +0400

    Added string of project name

--- gen/com/example/faltu/R.java
+++ gen/com/example/faltu/R.java
@@ -23,7 +23,6 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int logo=0x7f020001;
-
         public static final int fon1=0x7f020002;
         public static final int fon2=0x7f020003;
     }
